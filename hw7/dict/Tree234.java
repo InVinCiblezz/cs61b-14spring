7a186eaf16613230e0cecf140b1e68dc7226e733
InVinCiblezz <360340028@qq.com>
2020-03-29 04:57:38 -0500

    hw7 finished

---
+++ hw7/dict/Tree234.java
@@ -0,0 +1,279 @@
+/* Tree234.java */
+
+package dict;
+
+/**
+ *  A Tree234 implements an ordered integer dictionary ADT using a 2-3-4 tree.
+ *  Only int keys are stored; no object is associated with each key.  Duplicate
+ *  keys are not stored in the tree.
+ *
+ *  @author Jonathan Shewchuk
+ **/
+public class Tree234 extends IntDictionary {
+
+  /**
+   *  You may add fields if you wish, but don't change anything that
+   *  would prevent toString() or find() from working correctly.
+   *
+   *  (inherited)  size is the number of keys in the dictionary.
+   *  root is the root of the 2-3-4 tree.
+   **/
+  Tree234Node root;
+
+  /**
+   *  Tree234() constructs an empty 2-3-4 tree.
+   *
+   *  You may change this constructor, but you may not change the fact that
+   *  an empty Tree234 contains no nodes.
+   */
+  public Tree234() {
+    root = null;
+    size = 0;
+  }
+
+  /**
+   *  toString() prints this Tree234 as a String.  Each node is printed
+   *  in the form such as (for a 3-key node)
+   *
+   *      (child1)key1(child2)key2(child3)key3(child4)
+   *
+   *  where each child is a recursive call to toString, and null children
+   *  are printed as a space with no parentheses.  Here's an example.
+   *      ((1)7(11 16)22(23)28(37 49))50((60)84(86 95 100))
+   *
+   *  DO NOT CHANGE THIS METHOD.  The test code depends on it.
+   *
+   *  @return a String representation of the 2-3-4 tree.
+   **/
+  public String toString() {
+    if (root == null) {
+      return "";
+    } else {
+      /* Most of the work is done by Tree234Node.toString(). */
+      return root.toString();
+    }
+  }
+
+  /**
+   *  printTree() prints this Tree234 as a tree, albeit sideways.
+   *
+   *  You're welcome to change this method if you like.  It won't be tested.
+   **/
+  public void printTree() {
+    if (root != null) {
+      /* Most of the work is done by Tree234Node.printSubtree(). */
+      root.printSubtree(0);
+    }
+  }
+
+  /**
+   *  find() prints true if "key" is in this 2-3-4 tree; false otherwise.
+   *
+   *  @param key is the key sought.
+   *  @return true if "key" is in the tree; false otherwise.
+   **/
+  public boolean find(int key) {
+    Tree234Node node = root;
+    while (node != null) {
+      if (key < node.key1) {
+        node = node.child1;
+      } else if (key == node.key1) {
+        return true;
+      } else if ((node.keys == 1) || (key < node.key2)) {
+        node = node.child2;
+      } else if (key == node.key2) {
+        return true;
+      } else if ((node.keys == 2) || (key < node.key3)) {
+        node = node.child3;
+      } else if (key == node.key3) {
+        return true;
+      } else {
+        node = node.child4;
+      }
+    }
+    return false;
+  }
+
+
+  /**
+   *  insert() inserts the key "key" into this 2-3-4 tree.  If "key" is
+   *  already present, a duplicate copy is NOT inserted.
+   *
+   *  @param key is the key sought.
+   *             [2] Entry insert(Object k, Object e);
+   *
+   * insert(), like find(), walks down the tree in search of the key k. If it finds
+   * an entry with key k, it proceeds to that entry’s "left child" and continues.
+   * Unlike find(), insert() sometimes modifies         ----        -------
+   * nodes of the tree as it walks down.                |20|        |11 20|
+   * Specifically, whenever insert() encounters         ----        -------
+   * a 3-key node, the middle key is ejected,           /   \  =>    / |  \
+   * and is placed in the parent node instead.   ========== ---- ---- ---- ----
+   * Since the parent was previously treated the +10 11 12+ |30| |10| |12| |30|
+   * same way, the parent has at most two keys,  ========== ---- ---- ---- ----
+   * and always has room for a third. The other
+   * two keys in the 3-key node are split into two separate 1-key nodes, which are
+   * divided underneath the old middle key (as the figure illustrates).
+   * For example, suppose we                              ----
+   * insert 60 into the tree                              |40|
+   * depicted in [1]. The                               /------\
+   * first node visited is                        /---/         \----\
+   * the root, which has three                 ----                  ----
+   * keys; so we kick the                      |20|                  |50|
+   * middle key (40) upstairs.                 ----                /------\
+   * Since the root node has                  /    \              /        \
+   * no parent, a new node                ----      ----     ----         ----------
+   * is created to hold 40                |14|      |32|     |43|         |62 70 79|
+   * and becomes the root.                ----      ----     ----         ----------
+   * Similarly, 62 is kicked              /  \     /   \     /   \        /  |   |   \
+   * upstairs when insert()           ---- ---- ---- ---- ---- ---- ------- ---- ---- ----
+   * finds the node containing        |10| |18| |25| |33| |42| |47| |57 60| |66| |74| |81|
+   * it. This ensures us that         ---- ---- ---- ---- ---- ---- ------- ---- ---- ----
+   * when we arrive at the leaf
+   * (labeled 57 in this example), there’s room to add the new key 60.
+   * Observe that along the way, we created a new 3-key node "62 70 79". We do not
+   * kick its middle key upstairs until the next time it is visited.
+   * Again, the reasons why we split every 3-key node we encounter (and move its
+   * middle key up one level) are (1) to make sure there’s room for the new key in
+   * the leaf node, and (2) to make sure that above the leaves, there’s room for any
+   * key that gets kicked upstairs. Sometimes, an insertion operation increases the
+   * height of the tree by one by creating a new root.
+   **/
+  public void insert(int key) {
+    // Fill in your solution here.
+    // insert a root
+    Tree234Node node = root;
+    if (node == null) {
+      root = new Tree234Node(null, key);
+    } else {
+      while (node != null) {
+        if (node.isDuplicate(key)) return;
+        if (node.keys == 3) node = node.reconstuct();
+        if (node.isLeaf()) {
+          node.insert(key);
+          break;
+        }
+        node = node.goDown(key);
+      }
+    }
+    size++;
+  }
+
+  /**
+   *  testHelper() prints the String representation of this tree, then
+   *  compares it with the expected String, and prints an error message if
+   *  the two are not equal.
+   *
+   *  @param correctString is what the tree should look like.
+   **/
+  public void testHelper(String correctString) {
+    String treeString = toString();
+    System.out.println(treeString);
+    if (!treeString.equals(correctString)) {
+      System.out.println("ERROR:  Should be " + correctString);
+    }
+  }
+
+  /**
+   *  main() is a bunch of test code.  Feel free to add test code of your own;
+   *  this code won't be tested or graded.
+   **/
+  public static void main(String[] args) {
+    Tree234 t = new Tree234();
+
+    System.out.println("\nInserting 84.");
+    t.insert(84);
+    t.testHelper("84");
+
+    System.out.println("\nInserting 7.");
+    t.insert(7);
+    t.testHelper("7 84");
+
+    System.out.println("\nInserting 22.");
+    t.insert(22);
+    t.testHelper("7 22 84");
+
+    System.out.println("\nInserting 95.");
+    t.insert(95);
+    t.testHelper("(7)22(84 95)");
+
+    System.out.println("\nInserting 50.");
+    t.insert(50);
+    t.testHelper("(7)22(50 84 95)");
+
+    System.out.println("\nInserting 11.");
+    t.insert(11);
+    t.testHelper("(7 11)22(50 84 95)");
+
+    System.out.println("\nInserting 37.");
+    t.insert(37);
+    t.testHelper("(7 11)22(37 50)84(95)");
+
+    System.out.println("\nInserting 60.");
+    t.insert(60);
+    t.testHelper("(7 11)22(37 50 60)84(95)");
+
+    System.out.println("\nInserting 1.");
+    t.insert(1);
+    t.testHelper("(1 7 11)22(37 50 60)84(95)");
+
+    System.out.println("\nInserting 23.");
+    t.insert(23);
+    t.testHelper("(1 7 11)22(23 37)50(60)84(95)");
+
+    System.out.println("\nInserting 16.");
+    t.insert(16);
+    t.testHelper("((1)7(11 16)22(23 37))50((60)84(95))");
+
+    System.out.println("\nInserting 100.");
+    t.insert(100);
+    t.testHelper("((1)7(11 16)22(23 37))50((60)84(95 100))");
+
+    System.out.println("\nInserting 28.");
+    t.insert(28);
+    t.testHelper("((1)7(11 16)22(23 28 37))50((60)84(95 100))");
+
+    System.out.println("\nInserting 86.");
+    t.insert(86);
+    t.testHelper("((1)7(11 16)22(23 28 37))50((60)84(86 95 100))");
+
+    System.out.println("\nInserting 49.");
+    t.insert(49);
+    t.testHelper("((1)7(11 16)22(23)28(37 49))50((60)84(86 95 100))");
+
+    System.out.println("\nInserting 81.");
+    t.insert(81);
+    t.testHelper("((1)7(11 16)22(23)28(37 49))50((60 81)84(86 95 100))");
+
+    System.out.println("\nInserting 51.");
+    t.insert(51);
+    t.testHelper("((1)7(11 16)22(23)28(37 49))50((51 60 81)84(86 95 100))");
+
+    System.out.println("\nInserting 99.");
+    t.insert(99);
+    t.testHelper("((1)7(11 16)22(23)28(37 49))50((51 60 81)84(86)95(99 100))");
+
+    System.out.println("\nInserting 75.");
+    t.insert(75);
+    t.testHelper("((1)7(11 16)22(23)28(37 49))50((51)60(75 81)84(86)95" +
+                 "(99 100))");
+
+    System.out.println("\nInserting 66.");
+    t.insert(66);
+    t.testHelper("((1)7(11 16)22(23)28(37 49))50((51)60(66 75 81))84((86)95" +
+                 "(99 100))");
+
+    System.out.println("\nInserting 4.");
+    t.insert(4);
+    t.testHelper("((1 4)7(11 16))22((23)28(37 49))50((51)60(66 75 81))84" +
+                 "((86)95(99 100))");
+
+    System.out.println("\nInserting 80.");
+    t.insert(80);
+    t.testHelper("(((1 4)7(11 16))22((23)28(37 49)))50(((51)60(66)75" +
+                 "(80 81))84((86)95(99 100)))");
+
+    System.out.println("\nFinal tree:");
+    t.printTree();
+  }
+}
