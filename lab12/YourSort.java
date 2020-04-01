301be1e7da02eeeb5e34dfa48d882ff140c518a1
InVinCiblezz <360340028@qq.com>
2020-03-31 20:59:40 -0500

    lab12 finished

--- lab12/YourSort.java
+++ lab12/YourSort.java
@@ -4,5 +4,10 @@ public class YourSort {
 
   public static void sort(int[] A) {
     // Place your Part III code here.
+    if(A.length < 50) {
+      Sort.insertionSort(A);
+    } else {
+      Sort.quicksort(A);
+    }
   }
 }
